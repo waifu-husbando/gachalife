6511a6b1f342935ef4f566f5c517e501401586fd
CSMENG211 <bzhao920211@gmail.com>
2023-05-09 09:50:06 -0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -3,16 +3,11 @@ const CONFIG = {
   profile: {
     name: "morethanmin",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
-    instagram: "",
+    email: "blora2333@gmail.com",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `gachainfo`,
       href: "https://github.com/morethanmin/morethan-log",
     },
   ],
@@ -24,7 +19,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
+  link: "https://gachalife.info",
   since: 2022, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
