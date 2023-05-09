77622a75e82192a18c8920c9b702cd3626a660bf
CSMENG211 <bzhao920211@gmail.com>
2023-05-09 10:14:05 -0700

    test

--- site.config.js
+++ site.config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Ryan Z",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Gacha Gamer",
+    bio: "All Gachas are good",
+    email: "blora2333@gmail.com",
+    linkedin: "",
+    github: "",
     instagram: "",
   },
   projects: [
