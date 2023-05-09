8c82d434d3004a2e059f1bd8dc4231532580b5ea
CSMENG211 <bzhao920211@gmail.com>
2023-05-09 11:14:41 -0700

    add avatar

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "Ryan Z",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar.jpg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Gacha Gamer",
     bio: "No Waifu No Life",
     email: "",
